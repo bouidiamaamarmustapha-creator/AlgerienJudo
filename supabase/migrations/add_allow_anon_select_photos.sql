e1b4e41067b2a6f3b2287573fd02cbb9aa85f2f6
bouidiamaamarmustapha-creator <bouidiamaamarmustapha@gmail.com>
2025-10-18 01:06:09 +0100

    Delete supabase directory

--- supabase/migrations/add_allow_anon_select_photos.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Add "Allow anon select" policy to photos table
-*/
-CREATE POLICY "Allow anon select"
-  ON photos
-  FOR SELECT
-  TO authenticated
-  USING (true);
--- supabase/migrations/allow_anon_select_athletes.sql
+++
@@ -1,7 +0,0 @@
-# Allow select for anon on athletes table
-
-## Description
-- Allow select for anon on `athletes` table
-
-CREATE POLICY "Allow anon select" ON athletes FOR
-SELECT TO anon USING (true);
